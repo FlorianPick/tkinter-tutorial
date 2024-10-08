353b98666d2e8d1a857c2f9d5dc1d48d3e491b97
Florian Pick <mail@florian-pick.de>
2024-10-08 04:44:24 +0200

    Changed function to lowercase

--- calc.py
+++ calc.py
@@ -13,7 +13,7 @@ entry = tk.Entry(root, font=font_general, borderwidth=20, justify='right')
 entry.grid(row=0, column=0, columnspan=4)
 
 
-def addChar(char):
+def add_char(char):
 	global last_result
 	if entry.get() == str(last_result):
 		clear_entry()
@@ -36,45 +36,45 @@ def show_result():
 		entry.insert(0, "Error")
 
 
-button1 = tk.Button(root, text="1", font=font_general, command=lambda: addChar("1"))
-button2 = tk.Button(root, text="2", font=font_general, command=lambda: addChar("2"))
-button3 = tk.Button(root, text="3", font=font_general, command=lambda: addChar("3"))
-button4 = tk.Button(root, text="4", font=font_general, command=lambda: addChar("4"))
-button5 = tk.Button(root, text="5", font=font_general, command=lambda: addChar("5"))
-button6 = tk.Button(root, text="6", font=font_general, command=lambda: addChar("6"))
-button7 = tk.Button(root, text="7", font=font_general, command=lambda: addChar("7"))
-button8 = tk.Button(root, text="8", font=font_general, command=lambda: addChar("8"))
-button9 = tk.Button(root, text="9", font=font_general, command=lambda: addChar("9"))
-button0 = tk.Button(root, text="0", font=font_general, command=lambda: addChar("0"))
+button1 = tk.Button(root, text="1", font=font_general, command=lambda: add_char("1"))
+button2 = tk.Button(root, text="2", font=font_general, command=lambda: add_char("2"))
+button3 = tk.Button(root, text="3", font=font_general, command=lambda: add_char("3"))
+button4 = tk.Button(root, text="4", font=font_general, command=lambda: add_char("4"))
+button5 = tk.Button(root, text="5", font=font_general, command=lambda: add_char("5"))
+button6 = tk.Button(root, text="6", font=font_general, command=lambda: add_char("6"))
+button7 = tk.Button(root, text="7", font=font_general, command=lambda: add_char("7"))
+button8 = tk.Button(root, text="8", font=font_general, command=lambda: add_char("8"))
+button9 = tk.Button(root, text="9", font=font_general, command=lambda: add_char("9"))
+button0 = tk.Button(root, text="0", font=font_general, command=lambda: add_char("0"))
 
-button_add = tk.Button(root, text="+", font=font_general, command=lambda: addChar("+"))
-button_sub = tk.Button(root, text="-", font=font_general, command=lambda: addChar("-"))
-button_mul = tk.Button(root, text="*", font=font_general, command=lambda: addChar("*"))
-button_div = tk.Button(root, text="/", font=font_general, command=lambda: addChar("/"))
+button_add = tk.Button(root, text="+", font=font_general, command=lambda: add_char("+"))
+button_sub = tk.Button(root, text="-", font=font_general, command=lambda: add_char("-"))
+button_mul = tk.Button(root, text="*", font=font_general, command=lambda: add_char("*"))
+button_div = tk.Button(root, text="/", font=font_general, command=lambda: add_char("/"))
 
 button_res = tk.Button(root, text="=", font=font_general, command=show_result)
 button_clr = tk.Button(root, text="Clear", font=font_general, command=lambda: entry.delete(0, tk.END))
 
-button7.grid(row=1, column=0, sticky="nswe")
-button8.grid(row=1, column=1, sticky="nswe")
-button9.grid(row=1, column=2, sticky="nswe")
+button7.grid(row=1, column=0, sticky="nsew")
+button8.grid(row=1, column=1, sticky="nsew")
+button9.grid(row=1, column=2, sticky="nsew")
 
-button4.grid(row=2, column=0, sticky="nswe")
-button5.grid(row=2, column=1, sticky="nswe")
-button6.grid(row=2, column=2, sticky="nswe")
+button4.grid(row=2, column=0, sticky="nsew")
+button5.grid(row=2, column=1, sticky="nsew")
+button6.grid(row=2, column=2, sticky="nsew")
 
-button1.grid(row=3, column=0, sticky="nswe")
-button2.grid(row=3, column=1, sticky="nswe")
-button3.grid(row=3, column=2, sticky="nswe")
+button1.grid(row=3, column=0, sticky="nsew")
+button2.grid(row=3, column=1, sticky="nsew")
+button3.grid(row=3, column=2, sticky="nsew")
 
-button_mul.grid(row=4, column=0, sticky="nswe")
-button0.grid(row=4, column=1, sticky="nswe")
-button_div.grid(row=4, column=2, sticky="nswe")
+button_mul.grid(row=4, column=0, sticky="nsew")
+button0.grid(row=4, column=1, sticky="nsew")
+button_div.grid(row=4, column=2, sticky="nsew")
 
-button_clr.grid(row=1, column=3, sticky="nswe")
-button_sub.grid(row=2, column=3, sticky="nswe")
-button_add.grid(row=3, column=3, sticky="nswe")
-button_res.grid(row=4, column=3, sticky="nswe")
+button_clr.grid(row=1, column=3, sticky="nsew")
+button_sub.grid(row=2, column=3, sticky="nsew")
+button_add.grid(row=3, column=3, sticky="nsew")
+button_res.grid(row=4, column=3, sticky="nsew")
 
 for column in range(4):
 	root.grid_columnconfigure(column, weight=1)
